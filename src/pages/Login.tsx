514dde728bf746009f70dac0dea7de23dc1e61e9
berhab-zakarya <zakarya.berhab@univ-tlemcen.dz>
2025-04-19 16:46:58 +0100

    update

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import  { useEffect, useState } from 'react';
 
 export default function LoginComponent() {
   const [userInput, setUserInput] = useState('');
